aac34cb22f7bc0b90e14f3ab2cb504d060ca8b86
wellssam100 <wellssam100@gmail.com>
2025-03-02 22:24:20 -0500

    Completed header file

--- component_libraries/ultrasonic/ultrasonic.h
+++ component_libraries/ultrasonic/ultrasonic.h
@@ -1,30 +1,64 @@
+/**
+ * @file ultrasonic.h
+ * @defgroup ultrasonic ultrasonic
+ *  This library should allow user to activate the sensor and return the distance desired.
+ * 
+ */
+
 // #include guard
 #ifndef __ULTRASONIC_H__
 #define __ULTRASONIC_H__
 
-
-
-//Getting the build from the sdkconfig 
-// This needs to be specific to a project, not sure how to make this a library
-#if CONFIG_IDF_TARGET_ESP32
-#include <esp32/rom/ets_sys.h>
-/*
-#elif CONFIG_IDF_TARGET_ESP32S2
-#include <esp32s2/rom/ets_sys.h>
-#elif CONFIG_IDF_TARGET_ESP32S3
-#include <esp32s3/rom/ets_sys.h>
-#elif CONFIG_IDF_TARGET_ESP32C3
-#include <esp32c3/rom/ets_sys.h>
-#elif CONFIG_IDF_TARGET_ESP32H2
-#include <esp32h2/rom/ets_sys.h>
-#elif CONFIG_IDF_TARGET_ESP32C2
-#include <esp32c2/rom/ets_sys.h>
-*/
-#endif
-
-
-#include <freertos/FreeRTOS.h>
-#include <esp_idf_version.h>
-
+//Needed to add gpio functionality in turning on and off the trigger pin 
 #include <driver/gpio.h>
 #include <esp_err.h>
+
+//Error Definitions
+#define ESP_ERR_ULTRASONIC_PING         0x200
+#define ESP_ERR_ULTRASONIC_PING_TIMEOUT 0x201
+#define ESP_ERR_ULTRASONIC_ECHO_TIMEOUT 0x202
+
+/**
+ * Device Descriptor
+ * I don't think that I need any additional info besides signal listeners and senders
+ */
+typedef struct ultrasonic_t
+{
+    gpio_num_t trigger_pin;
+    gpio_num_t echo_pin;
+};
+
+/**
+ * 
+ * @brief Setup GPIO pins of the ultrascale and set the trigger pin to low
+ * @param dev Pointer to the device descriptor 
+ * @return `ESP_OK` on success
+ * 
+ */
+esp_err_t ultrasonic_init(const ultrasonic_t *dev);
+
+/**
+ * @brief Send an ultrasonic pulse and return the time between echo and receive
+ * @param dev
+ * @param max_wait fsdf
+ * @param[out] time time variable in microseconds. This variable will be changed in the function running and won't have to be returned
+ * 
+ * @return `ESP_OK` on success, otherwise:
+ *         - ::ESP_ERR_ULTRASONIC_PING         - Invalid state (previous ping is not ended)
+ *         - ::ESP_ERR_ULTRASONIC_PING_TIMEOUT - Device is not responding
+ *         - ::ESP_ERR_ULTRASONIC_ECHO_TIMEOUT - Distance is too big or wave is scattered
+ */
+esp_err_t ultrasonic_measure_time(const ultrasonic_t *dev, uint32_t max_wait, uint32_t time);
+
+/**
+ * @brief Turn the time measurement into a distance measurement
+ * @param max_dist should equal the distance of the max time
+ * @param[out] dist Distance in meters
+ * @return `ESP_OK` on success, otherwise:
+ *         - ::ESP_ERR_ULTRASONIC_PING         - Invalid state (previous ping is not ended)
+ *         - ::ESP_ERR_ULTRASONIC_PING_TIMEOUT - Device is not responding
+ *         - ::ESP_ERR_ULTRASONIC_ECHO_TIMEOUT - Distance is too big or wave is scattered
+ * 
+ */
+ esp_err_t ultrasonic_measure_distance(const ultrasonic_t *dev, uint32_t max_dist, uint32_t dist);
+
