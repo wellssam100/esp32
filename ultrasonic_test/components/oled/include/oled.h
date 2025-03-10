d801b34b9406c3940892f8d4b2d753aaf4cb12b0
wellssam100 <wellssam100@gmail.com>
2025-03-09 23:56:59 -0400

    oled library

---
+++ ultrasonic_test/components/oled/include/oled.h
@@ -0,0 +1,40 @@
+/**
+ * @file oled.h 
+ * This library should allow users to draw basic shapes onto an oled
+ * Create a device descriptor for ssd1036
+ */
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_system.h"
+#include <stdio.h>
+#include <stdint.h>
+
+
+typedef struct oled_dev_t{
+    //I2C handles
+    int i2c_addr; //device address
+    esp_lcd_panel_io_handle_t i2c_handle; 
+    esp_lcd_panel_io_i2c_config_t i2c_config;
+    esp_lcd_panel_handle_t panel_handle;
+    esp_lcd_panel_dev_config_t panel_config;
+    //Screen stuff
+    int max_height;//64
+    int max_width;//128s
+} oled_dev_t;
+
+/**
+ * @brief initialize the i2c and panel configurations
+ * @param oled_dev Oled Device structure
+ * @return ESP_OK on success, fail if any failed pointers
+ */
+esp_err_t oled_dev_init(oled_dev_t* oled_dev, i2c_master_bus_handle_t* master_bus, uint32_t oled_i2c_addr);
+
+//Do I need to initialize the aspect ratio? Range for x and y?
+
+
+/**
+ * @
+ * 
+ */
+esp_err_t oled_draw_dot(const oled_dev_t* oled_dev, uint8_t x, uint8_t y);
---
+++ ultrasonic_test/components/oled/oled.c
@@ -0,0 +1,84 @@
+/**
+ * @file Source for oled 
+ */
+#include <stdio.h>
+#include "include/oled.h"
+#include <esp_err.h>
+#include "driver/gpio.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include <stdint.h>
+
+
+#define TEST_LCD_PIXEL_CLOCK_HZ (400 * 1000)
+#define HEIGHT 64
+#define WIDTH 128
+
+
+//init the device
+
+esp_err_t oled_dev_init(oled_dev_t* oled_dev, i2c_master_bus_handle_t* master_bus, uint32_t oled_i2c_addr)
+{
+    //Check to see if the passed arg is null
+    // I should only need 2 pins, I am using i2c to communicate with the board
+    if(!(oled_dev)||!(master_bus)){
+        return ESP_ERR_INVALID_ARG;
+    }
+    //There is a handle and a config for both the io and the panel, all 4 need to be setup  
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t io_config = {
+        .dev_addr = oled_i2c_addr,
+        .scl_speed_hz = 1000,//TEST_LCD_PIXEL_CLOCK_HZ,
+        .control_phase_bytes = 1, // According to SSD1306 datasheet
+        .dc_bit_offset = 6,       // According to SSD1306 datasheet
+        .lcd_cmd_bits = 8,        // According to SSD1306 datasheet
+        .lcd_param_bits = 8,      // According to SSD1306 datasheet
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(*master_bus, &io_config, &io_handle));
+        
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .bits_per_pixel = 1,
+        .reset_gpio_num = -1,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+    //set up struct
+    oled_dev->i2c_config = io_config;
+    oled_dev->i2c_handle = io_handle;
+    oled_dev->panel_config = panel_config;
+    oled_dev->panel_handle = panel_handle;
+    oled_dev->max_height = HEIGHT;
+    oled_dev->max_width = WIDTH;
+    
+    
+    //prep screen
+    ESP_LOGI("OLED", "%p",panel_handle);
+    ESP_LOGI("OLED", "%p",oled_dev->panel_handle);
+
+    return ESP_OK;
+    
+}
+
+esp_err_t oled_draw_dot(const oled_dev_t* oled_dev, uint8_t x, uint8_t y){
+    static uint16_t s_white_buffer[WIDTH *HEIGHT] ;
+    for (int i = 0; i < WIDTH * HEIGHT; i++) {
+        s_white_buffer[i] = 0xFFFF;  // White pixel in RGB565
+    }
+    static const uint16_t s_dot_data[2] = {
+        0x0003,0x0003
+    };
+
+    static uint16_t s_black_buffer[2] = {0};
+    //ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(oled_dev->panel_handle,x_start,y_start,x_start+2,y_start+2,s_dot_data));
+    ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(oled_dev->panel_handle,x,y,x+2,y+4,&s_dot_data));
+    ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(oled_dev->panel_handle,x-2,y,x,y+4,&s_black_buffer));
+    //ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(oled_dev->panel_handle,x,y,x+2,y+4,&s_black_buffer));
+
+    return ESP_OK;
+}
